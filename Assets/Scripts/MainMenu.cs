0625dc3d7129fabdb44b068fe5edc0706e94053b
Neetesh Dadwariya <neeteshdadwariya@gmail.com>
2022-12-06 09:44:03 -0600

    Orbital Chemistry Done

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitButton(){
+        Application.Quit();
+        Debug.Log("application closed");
+    }
+
+    public void onAtomicAudioVisualizationClicked(){
+        SceneManager.LoadScene("AtomicAudioBeatSynchronization");
+    }
+    
+    public void onMolecularStructureClicked(){
+        SceneManager.LoadScene("MolecularStructure");
+    }
+    
+    public void onObritalChemistryClicked(){
+        SceneManager.LoadScene("OrbitalChemistry");
+    }
+}
